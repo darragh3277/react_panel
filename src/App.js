30ebcd1a6e1af3645edd6457dca42cb467e0be5c
Darragh O'Brien <darragh3277@gmail.com>
2020-01-16 10:07:42 +0000

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {routes} from './Routes'
 import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,11 +15,20 @@ class App extends React.Component {
       <>
         <Router>
           <Navbar />
-            <Switch>
-              {routes.map((route,i)=>
-                <Route key={i} path={route.path} component={route.component} exact></Route>
-              )}
-            </Switch>
+          <Switch>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-3 col-lg-2">
+                  <Sidebar />
+                </div>
+                <div className="col-md-9 col-lg-10">
+                  {routes.map((route,i)=>
+                    <Route key={i} path={route.path} component={route.component} exact></Route>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Switch>
         </Router>
       </>
     );
