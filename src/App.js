f87b827bd288917e38b31c431bf120f40dddd4d4
Darragh O'Brien <darragh3277@gmail.com>
2020-01-16 10:22:25 +0000

    make containers 100%

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Route,
   Switch
 } from "react-router-dom";
+import './App.css';
 
 class App extends React.Component {
 
@@ -16,12 +17,12 @@ class App extends React.Component {
         <Router>
           <Navbar />
           <Switch>
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-md-3 col-lg-2">
+            <div className="container-fluid h-100">
+              <div className="row h-100">
+                <div className="col-md-3 col-lg-2 border">
                   <Sidebar />
                 </div>
-                <div className="col-md-9 col-lg-10">
+                <div className="col-md-9 col-lg-10 border">
                   {routes.map((route,i)=>
                     <Route key={i} path={route.path} component={route.component} exact></Route>
                   )}
