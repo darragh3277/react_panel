78a75cc47bdee191a0b0e863a4a06f0d05d7b888
Darragh O'Brien <darragh3277@gmail.com>
2020-01-17 08:59:34 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {routes} from './Routes'
 import Navbar from './components/navigation/navbar/Navbar';
 import Sidebar from './components/navigation/sidebar/Sidebar';
+import Footer from './components/footer/Footer';
 import {
   BrowserRouter as Router,
   Route,
@@ -19,12 +20,23 @@ class App extends React.Component {
             <div className="container-fluid">
               <div className="row">
                 <Sidebar />
-                <main className="col-md-9 col-lg-10 pt-3 px-4">
-                  <Switch>
-                    {routes.map((route,i)=>
-                      <Route key={i} path={route.path} component={route.component} exact></Route>
-                    )}
-                  </Switch>
+                <main className="col-md-9 col-lg-10 pt-3 px-0">
+                  <div className="container-fluid">
+                    <div className="row px-4" id="main-content">
+                      <div className="col">
+                        <Switch>
+                          {routes.map((route,i)=>
+                            <Route key={i} path={route.path} component={route.component} exact></Route>
+                          )}
+                        </Switch>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col" id="footer">
+                        <Footer />
+                      </div>
+                    </div>
+                  </div>
                 </main>
               </div>
             </div>
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render () {
+    return (
+      <div className="bg-dark text-light p-1 pr-4 text-right">
+            Powered by <a className="text-info" href="https://github.com/darragh3277/react_panel">React Panel</a>
+      </div>
+    );
+  }
+
+}
+
+export default Footer;
