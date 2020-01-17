6e6c57bec161c299f17e5a1ad7f3ee4d7fb012a9
Darragh O'Brien <darragh3277@gmail.com>
2020-01-17 15:57:38 +0000

    fixing minor html errors

--- src/components/main/cards/account_details/AccountDetails.js
+++ src/components/main/cards/account_details/AccountDetails.js
@@ -10,39 +10,39 @@ class AccountDetails extends React.Component {
                             <h6>Account Details</h6>
                         </div>
                         <form className="pt-3">
-                        <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label className="font-weight-bold" for="firstName">First Name</label>
-                                    <input type="text" id="firstName" class="form-control form-control-sm" />
+                        <div className="form-row">
+                                <div className="form-group col-md-6">
+                                    <label className="font-weight-bold" htmlFor="firstName">First Name</label>
+                                    <input type="text" id="firstName" className="form-control form-control-sm" />
                                 </div>
-                                <div class="form-group col-md-6">
-                                    <label className="font-weight-bold" for="lastName">Last Name</label>
-                                    <input type="text" id="lastName" class="form-control form-control-sm" />
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col">
-                                    <label className="font-weight-bold" for="email">Email</label>
-                                    <input type="email" id="email" class="form-control form-control-sm" />
+                                <div className="form-group col-md-6">
+                                    <label className="font-weight-bold" htmlFor="lastName">Last Name</label>
+                                    <input type="text" id="lastName" className="form-control form-control-sm" />
                                 </div>
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label className="font-weight-bold" for="new_password">New Password</label>
-                                    <input type="password" id="new_password" class="form-control form-control-sm" />
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label className="font-weight-bold" for="old_password">Confirm Password</label>
-                                    <input type="password" id="old_password" class="form-control form-control-sm" />
+                            <div className="form-row">
+                                <div className="form-group col">
+                                    <label className="font-weight-bold" htmlFor="email">Email</label>
+                                    <input type="email" id="email" className="form-control form-control-sm" />
                                 </div>
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col">
-                                    <label className="font-weight-bold" for="description">Description</label>
-                                    <textarea class="form-control form-control-sm" id="description" rows="3"></textarea>
+                            <div className="form-row">
+                                <div className="form-group col-md-6">
+                                    <label className="font-weight-bold" htmlFor="new_password">New Password</label>
+                                    <input type="password" id="new_password" className="form-control form-control-sm" />
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label className="font-weight-bold" htmlFor="old_password">Confirm Password</label>
+                                    <input type="password" id="old_password" className="form-control form-control-sm" />
                                 </div>
                             </div>
-                            <button class="btn btn-primary btn-sm">Update Account</button>
+                            <div className="form-row">
+                                <div className="form-group col">
+                                    <label className="font-weight-bold" htmlFor="description">Description</label>
+                                    <textarea className="form-control form-control-sm" id="description" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <button className="btn btn-primary btn-sm float-right">Update Account</button>
                         </form>
                     </div>
                 </div>
