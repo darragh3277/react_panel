0b0111e90ada5565c494dc62e3c4ab94b09f8d4a
Darragh <darragh3277@gmail.com>
2020-01-20 20:13:53 +0000

    add load more button to messages card

--- src/components/main/cards/messages/Messages.js
+++ src/components/main/cards/messages/Messages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Messages.css';
 
 const messages = [
   {
@@ -28,37 +29,53 @@ const messages = [
 class Messages extends React.Component {
   renderMessages = () => {
     return (
-      <ul className="list-group list-group-flush">
-        {messages.map(message => {
-          const { id } = message;
-          return (
-            <div className="pb-0" key={id}>
-              <li className="list-group-item border-bottom my-0 list-group-item-action cursor-pointer">
-                <div className="row">
-                  <div className="col-12">
-                    <strong className="text-info">{message.from}</strong>
-                    <small className="text-warning float-right">
-                      {message.date}
-                    </small>
-                    <div className="col-12 text-truncate d-inline-block pl-0">
-                      {message.content}
+      <div className="pt-2 row">
+        <div className="col-12">
+          <ul className="list-group list-group-flush">
+            {messages.map(message => {
+              const { id } = message;
+              return (
+                <li
+                  key={id}
+                  className="pb-0 list-group-item my-0 list-group-item-action border-bottom cursor-pointer"
+                >
+                  <div className="row">
+                    <div className="col-12">
+                      <strong className="text-info">{message.from}</strong>
+                      <small className="text-warning float-right">
+                        {message.date}
+                      </small>
+                      <div className="col-12 text-truncate d-inline-block pl-0">
+                        {message.content}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            </div>
-          );
-        })}
-      </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  renderLoadMore = () => {
+    return (
+      <div className="pt-2 row justify-content-center">
+        <button type="button" className="align-center btn btn-primary btn-sm">
+          Load more
+        </button>
+      </div>
     );
   };
 
   render() {
     return (
       <>
-        <div className="card card-small">
+        <div className="card card-small" id="messages-card">
           <div className="card-body pt-1">
-            <div className="pt-3">{this.renderMessages()}</div>
+            {this.renderMessages()}
+            {this.renderLoadMore()}
           </div>
         </div>
       </>
